7c6772d048d25276b975832f5959f97c3ef2de56
Ba5ik7 <dusellfam@gmail.com>
2019-04-21 14:20:07 -0400

    soap shit

--- api/controllers/SoapController.js
+++ api/controllers/SoapController.js
@@ -4,11 +4,25 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+const soap = require('soap');
 module.exports = {
   
     async hello(req, res) {
         res.ok();
+    },
+
+
+    async someSoapCall(req, res) {
+        console.log('Hello');
+        soap.createClient('http://demo4006214.mockable.io/hello?wsdl', (err, client) => {
+            if(err) return res.send({ status: 500, err });            
+            // res.send({ status: client.wsdl.xml });
+            client.getSomeData('', (err, data) => {
+                // if(err) return res.send({ status: 500, err });
+                console.log(err, data);
+                res.send({ status: 200 });
+            });
+        });
     }
 };
 
