a8f9fa2664520380c5013e13291cf494cce862bd
Ba5ik7 <dusellfam@gmail.com>
2019-04-21 18:23:38 -0400

    XML to JS

--- api/controllers/SoapController.js
+++ api/controllers/SoapController.js
@@ -5,22 +5,27 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 const soap = require('soap');
+const convert = require('xml-js');
+
 module.exports = {
   
     async hello(req, res) {
         res.ok();
     },
 
-
     async someSoapCall(req, res) {
-        console.log('Hello');
         soap.createClient('http://demo4006214.mockable.io/hello?wsdl', (err, client) => {
-            if(err) return res.send({ status: 500, err });            
-            // res.send({ status: client.wsdl.xml });
-            client.getSomeData('', (err, data) => {
+            if(err) return res.send({ status: 500, err });
+
+            client.getSomeData({ description: 'YOOOOOOOOO' }, (err, result) => {
                 // if(err) return res.send({ status: 500, err });
-                console.log(err, data);
-                res.send({ status: 200 });
+
+                const payload = result.toJSON();
+                let xml2js = convert.xml2js(payload.body, { compact: false });
+                xml2js.elements[0].elements[0].elements[0].elements[0].elements[0].text = 'This is shit';
+
+                const js2xml = convert.json2xml(xml2js);
+                res.send({ raw_body: payload.body, xml2js, js2xml });
             });
         });
     }
